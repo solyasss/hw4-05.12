523e7f9df9fe546ba9516662c3d4c9b95fa0fb06
olya <olgastet01032005@gmail.com>
2024-12-07 14:29:12 +0200

    hw4

---
+++ hw4 05.12/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace hw4_05._12;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
---
+++ hw4 05.12/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace hw4_05._12
+{
+    public partial class main_window : Window
+    {
+        public main_window()
+        {
+            InitializeComponent();
+        }
+        private void button_click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button)
+            {
+                string button_name = button.Content.ToString(); 
+                
+                switch (button_name)
+                {
+                    case "Создать":
+                        MessageBox.Show("Кнопка 'Создать' нажата");
+                        break;
+                    case "Изменить":
+                        MessageBox.Show("Кнопка 'Изменить' нажата");
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
